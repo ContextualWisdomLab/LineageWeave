f38313f1b453b6ac22719edbee19a34233757cb7
Seongho Bae <me@seonghobae.me>
2026-08-19 22:20:47 -0700

    feat(db): roll back versioned lineage evidence schema

--- migrations/rollback/0053_lineage_edge_channel_score.sql
+++ migrations/rollback/0053_lineage_edge_channel_score.sql
@@ -2,6 +2,15 @@ begin;
 
 drop table if exists lineage_edge_channel_score;
 
+drop index if exists post_lineage_edge_reconstruction_run_idx;
+alter table post_lineage_edge
+    drop constraint if exists post_lineage_edge_reconstruction_run_fk;
+alter table post_lineage_edge
+    drop column if exists lineage_reconstruction_run_id;
+
+drop table if exists lineage_reconstruction_run_channel;
+drop table if exists lineage_reconstruction_run;
+
 delete from common_lookup_value
  where lookup_category = 'lineage_channel'
    and lookup_code in (
