cf7331ae25544f660ecad11a4fc965f9cef107f1
Seongho Bae <me@seonghobae.me>
2026-08-21 09:42:39 +0900

    style: use deferred region result annotation

--- lineageweave/post_content_normalization.py
+++ lineageweave/post_content_normalization.py
@@ -66,7 +66,7 @@ class ImageContentResult:
     mime_type: str
     status_code: str
     description: ImageDescription | None = None
-    regions: tuple["ImageRegionResult", ...] = field(default_factory=tuple)
+    regions: tuple[ImageRegionResult, ...] = field(default_factory=tuple)
 
 
 @dataclass(frozen=True)
