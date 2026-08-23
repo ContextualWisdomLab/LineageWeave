329449790cc65868063c32a82eac9ed777fe9b2f
Seongho Bae <me@seonghobae.me>
2026-08-24 05:37:42 +0900

    Remove stale migration replay alias

--- tests/test_migration_replay.py
+++ tests/test_migration_replay.py
@@ -34,5 +34,5 @@ def test_migrate_sh_replays_leftover_pair_migration_on_existing_volumes() -> Non
 
     assert "0012_*" in script
     assert "0103_*" in script
-    assert "0104_*" in script
+    assert "0104_*" not in script
     assert "0172_*" in script
